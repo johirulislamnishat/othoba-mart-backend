d037bcc939b2dc572980c604a9e84fa95cde56b6
Waizul Haque <waizul@gmail.com>
2022-04-03 17:01:44 +0600

    user model created

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors())
 app.use(express.json())
 
 //mongodb connection
-mongoose.connect(process.env.MONGO_URL).then( console.log('mongodb connected')).catch(err=>console.log(err))
+mongoose.connect(process.env.MONGO_URL)
+.then( console.log('mongodb connected'))
+.catch(err=>console.log(err))
 
 
 
