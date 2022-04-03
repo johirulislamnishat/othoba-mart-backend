db5a1659aba6d130cf278e48237776528f7a430b
Waizul Haque <waizul@gmail.com>
2022-04-03 19:06:25 +0600

    login a user

--- routes/auth.js
+++ routes/auth.js
@@ -13,11 +13,28 @@ router.post('/register', async(req,res)=>{
     })
 
     try{
+        //save data to mongodb
         const savedUser = await newUser.save()
+
+        //send respond to client
         res.status(201).json(savedUser)
     } catch (err) {
         res.status(500).json(err)
     }
+})
+
+//login
+router.post('/login', async(req,res)=>{
+try{
+    //finding user
+    const user = await User.findOne( { email: req.body.email} )
+    const { password, ...others} = user._doc
+  
+    res.status(200).json(others)
+
+} catch(err){
+    res.status(500).json(err)
+}
 })
 
 module.exports = router
