66a05647e17c78b181ce543154a1e2ab2620042d
Waizul Haque <waizul@gmail.com>
2022-04-04 15:51:19 +0600

    deleting main from packagejson

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-require('dotenv').config()
-const cors = require('cors')
-const port = process.env.PORT || 5000
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-//route import 
-const authRoute = require('./routes/auth')
+//route import
+const authRoute = require("./routes/auth");
 
 //middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //mongodb connection
-mongoose.connect(process.env.MONGO_URL)
-.then( console.log('mongodb connected'))
-.catch(err=>console.log(err))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("mongodb connected"))
+  .catch((err) => console.log(err));
 
+  
 //api endpoint
-app.use('/auth', authRoute)
+app.use("/auth", authRoute);
 
+app.get("/", (req, res) => {
+  res.send("othoba-mart backend api");
+});
 
-
-app.get('/',(req,res)=>{
-    res.send('othoba-mart backend api')
-})
-
-app.listen(port, ()=>{
-    console.log('backend api is running on',port)
-})
+app.listen(port, () => {
+  console.log("backend api is running on", port);
+});
